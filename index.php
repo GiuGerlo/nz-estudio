dc6c70aae452f1b5a019556e56c9903d1c593d5e
giugerlo <ggiuliano526@gmail.com>
2025-05-21 19:39:28 -0300

    Pequeños ajustes en el hero

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
   include_once 'includes/head.php';
   include_once 'templates/hero.php';
+  include_once 'templates/about.php';
   include_once 'includes/footer.php';
 ?>
---
+++ templates/about.php
@@ -0,0 +1,47 @@
+<!-- About Section -->
+<section id="about" class="about section">
+
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gx-0 gx-lg-5 gy-5 align-items-center">
+            <div class="col-lg-6" data-aos="zoom-out" data-aos-delay="200">
+                <div class="image-wrapper">
+                    <div class="image-box">
+                        <img src="assets/img/logo-nadina.jpg" class="img-fluid" alt="About Image" loading="lazy">
+                    </div>
+                    <!-- <div class="experience-box" data-aos="zoom-in" data-aos-delay="300">
+                        <div class="years">15+</div>
+                        <div class="text">Years of<br>Experience</div>
+                    </div> -->
+                </div>
+            </div>
+
+            <div class="col-lg-6" data-aos="fade-left" data-aos-delay="200">
+                <div class="content">
+                    <div class="section-header">
+                        <span class="subtitle">Nosotros</span>
+                        <!-- <h2>Empowering Businesses Through Innovative Solutions</h2> -->
+                    </div>
+
+                    <div class="about-section">
+                        <p class="highlight-text"><span>NZ ESTUDIO JURIDICO-INMOBILIARIO</span> nació con la pandemia que azotó al mundo y nos encerró en el año 2020. Fue allí cuando sentí la necesidad de estudiar la carrera de Martillera, Corredora Pública y Corredora Inmobiliaria en la Universidad Blas Pascal de Córdoba, para ampliar mi Estudio Jurídico y brindarle al mercado inmobiliario un servicio de excelencia y confiabilidad para con los clientes.</p>
+                        <p class="highlight-text">Abogada de profesión, egresada en el Año 2002 de la Universidad Católica de Rosario, Facultad de Derecho y Ciencias Sociales, pude aportar a mis clientes un asesoramiento de calidad y confianza.</p>
+                        <p class="highlight-text">La idea fundamental es que los propietarios puedan deslindar sus preocupaciones en mi equipo de trabajo, ofreciendo respuestas eficaces a sus necesidades.</p>
+                        <p class="highlight-text">Con mucha dedicación, seriedad y constancia buscamos aportar a nuestra comunidad y a las de la zona, un granito de arena en la difícil realidad habitacional que nos aqueja.</p>
+                        <p class="highlight-text">Con el firme convencimiento de que “el saber no ocupa lugar”, buscamos día a día perfeccionarnos, corregir errores, superarnos para dar un mejor servicio a cada cliente, porque estoy convencida de que “las oportunidades no ocurren, las creas”.</p>
+                        <p class="highlight-text"><span>Dra. Nadina I. Zaranich <br>Abogada – Mat 13-257 <br>Corredora I. Mat. 6886 </span></p>
+                    </div>
+
+
+
+                    <!-- <div class="cta-buttons">
+                        <a href="#" class="btn-learn-more">Learn More</a>
+                        <a href="#" class="btn-get-started">Get Started</a>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section><!-- /About Section -->
